3e4f8add52b21910ae9ee4fb3a8ecf8eaf737d5e
nodeicode <lgopik2@uic.edu>
2019-07-04 18:45:33 +0530

    time for front end  designing!

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,15 @@
 import '../static/index.css'
 export default function HomePage() {
   return (
-    <div class="a">
+    <div class="r">
+      <div class="a">
       <img src="../static/profile.jpeg"/>
       <h1>Lohit Aryan</h1>
+      </div>
+      <div class="b">
+      <h1>What did I do?</h1>
+      <h2>That's what this page is all about <span>😁</span></h2>
+    </div>
     </div>
   )
 }
