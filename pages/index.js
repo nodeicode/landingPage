8a3919129c2caf0a17c58b3a41276231e2384ad1
nodeicode <lgopik2@uic.edu>
2019-07-11 00:52:41 +0530

    Basic  Animation need to optimize and smooth it

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,33 @@
 import '../static/index.css'
+import React from 'react'
 import About from '../src/components/About/About.js'
-export default function HomePage() {
-  return (
+import Typist from 'react-typist'
+export default class HomePage extends React.Component {
+
+  state = {render:false,class:""}
+  
+  renderAbout=()=>{
+    setTimeout(()=>{
+      this.setState({render:true,class:"dis"});
+    },2000);
+  }
+
+  render(){
+  const dis = {display:'none'}
+  return(<About/>)
+    /*return (
     <div className="root">
-      <About/>
+      <Typist
+        avgTypingDelay={100}
+        startDelay={2000}
+        cursor={{blink:true}}
+        onTypingDone={this.renderAbout}>
+        <a>HELLO!</a>
+      <Typist.Backspace count={6} delay={100}/>
+        </Typist>
+      {this.state.render?(<div><About/>
+      <style jsx global>{`.Typist{display:none}`}</style>
+      </div>):null}
     </div>
-  )
-}
+  )*/
+}}
--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,23 +1,30 @@
 import React from 'react'
-import styled from 'styled-components'
+import './About.css'
+import styled,{keyframes} from 'styled-components'
+import {fadeInRight} from 'react-animations'
+
+const fadein  = keyframes`${fadeInRight}`;
+
 const Card = styled.div`
 box-shadow: 0 4px 8px 0 rgb(0,0,0,0.2);
 border-radius:5%;
+animation: 2s ${fadein};
 background-color:white;
 border-type:block;
 border-color:white;
 `;
-export default function  About(){
+export default class About extends React.Component{
+  render(){
         return(
             <div className="r">
             <Card className="a">
             <img src="../static/profile.jpeg"/>
-            <h1>Lohit Aryan</h1>
+            <h1 className="hi">Lohit Aryan</h1>
             </Card>
             <Card className="b">
-            <h1>What did I Do?</h1>
+            <h1 className="hi">What did I Do?</h1>
             <h2>That's what this page is all about <span>😁</span></h2>
           </Card>
           </div>
         );
-    }
+    }}
