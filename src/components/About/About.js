5bdd78c1184faa2dd94916436c4f34181c70a757
nodeicode <lgopik2@uic.edu>
2019-08-07 15:09:10 +0530

    basic done! need finishing touches and a pic! :P

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -2,7 +2,7 @@ import React from 'react'
 import './About.css'
 import styled,{keyframes} from 'styled-components'
 import {fadeInRight} from 'react-animations'
-import {FaLinkedin,IconContext,FaTwitterSquare,FaGithub,FaDev} from 'react-icons'
+import '@fortawesome/fontawesome-free/js/all'
 
 const fadein  = keyframes`${fadeInRight}`;
 
@@ -15,7 +15,25 @@ border-type:solid;
 border-color:white;
 border-width:10px;
 `;
+const Icon = styled.span`
+font-size:5vmax;
+color:beige;
+animation: 2s ${fadein};
+`;
+const Linkw = styled.a`
+text-decoration:none;
+color:inherit;
+`; 
 export default class About extends React.Component{
+
+  state={show:false};
+
+  componentDidMount(){
+    setTimeout(()=>{
+      this.setState({show:true});
+    },2000);
+  }
+
   render(){
         return(
             <div className="r">
@@ -23,10 +41,11 @@ export default class About extends React.Component{
             <img src="../static/profile.jpg"/>
             <h1 className="hi">Lohit Aryan</h1>
             </Card>
-            <FaLinkedin className="L"/>
-            <FaTwitterSquare/>
-            <FaGithub/>
-            <FaDev/>
+            <Icon className="L I"><Linkw href="https://www.linkedin.com/in/lohit-aryan/"><i className="fab fa-linkedin"/></Linkw></Icon>
+            <Icon className="T I"><Linkw href="https://twitter.com/stackicode/"><i className="fab fa-twitter-square"/></Linkw></Icon>
+            <Icon className="D I"><Linkw href="https://dev.to/nodeicode"><i className="fab fa-dev"/></Linkw></Icon>
+            <Icon className="G I"><Linkw href="https://github.com/nodeicode"><i className="fab fa-github"/></Linkw></Icon>
+            {this.state.show?(<style global jsx>{`.I{display:block}.a{grid-column:2}`}</style>):null}
           </div>
         );
     }}
